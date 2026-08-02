84be7d4398e791e8fee23cd0b3a839cf85978f08
Christy Jose <107241938+christhe21@users.noreply.github.com>
2026-08-03 01:19:20 +0530

    Add material-icons-extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.compose.material:material-icons-extended")
 
     // WorkManager for daily wallpaper
     implementation(libs.androidx.work.runtime.ktx)
